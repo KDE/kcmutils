18dc36f089ae1f4fe404242ce359962168f16e83
Kai Uwe Broulik <kde@privat.broulik.de>
2025-06-08 14:04:26 +0200

    KCModuleData: Add auxiliaryAction and isRelevant
    This lets a KCM provide additional features to the user without
    having to the enter the KCM:
    
    * whether it is relevant in the current environment, e.g. whether the
      device type, such as a touchscreen or graphics tablet, is availbale
      in the first place
    * an auxiliary action, that can either be a button to e.g. pair a new
      device, or a switch to toggle a feature, e.g. turn on/off Bluetooth

--- src/core/kcmoduledata.cpp
+++ src/core/kcmoduledata.cpp
@@ -21,6 +21,8 @@ public:
 
     KCModuleData *_q;
     QList<QPointer<KCoreConfigSkeleton>> _skeletons;
+    QAction *auxiliaryAction = nullptr;
+    bool relevant = true;
 };
 
 KCModuleData::KCModuleData(QObject *parent)
@@ -77,6 +79,32 @@ bool KCModuleData::matchesQuery(const QString &query) const
     // Currently not implemented, here for future use case
     Q_UNUSED(query)
     return false;
+}
+
+QAction *KCModuleData::auxiliaryAction() const
+{
+    return d->auxiliaryAction;
+}
+
+void KCModuleData::setAuxiliaryAction(QAction *action)
+{
+    if (d->auxiliaryAction != action) {
+        d->auxiliaryAction = action;
+        Q_EMIT auxiliaryActionChanged(action);
+    }
+}
+
+bool KCModuleData::isRelevant() const
+{
+    return d->relevant;
+}
+
+void KCModuleData::setRelevant(bool relevant)
+{
+    if (d->relevant != relevant) {
+        d->relevant = relevant;
+        Q_EMIT relevantChanged(relevant);
+    }
 }
 
 #include "moc_kcmoduledata.cpp"
