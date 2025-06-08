d6cb5ae14a533784f8ab21e306b16d1daf2397c7
Kai Uwe Broulik <kde@privat.broulik.de>
2025-06-08 14:04:26 +0200

    KCModuleData: Add auxiliaryAction and isRelevant
    This lets a KCM provide additional features to the user without
    having to the enter the KCM:
    
    * whether it is relevant in the current environment, e.g. whether the
      device type, such as a touchscreen or graphics tablet, is availbale
      in the first place
    * an auxiliary action, that can either be a button to e.g. pair a new
      device, or a switch to toggle a feature, e.g. turn on/off Bluetooth

--- src/core/kcmoduledata.h
+++ src/core/kcmoduledata.h
@@ -15,6 +15,8 @@
 class KCModuleDataPrivate;
 class KCoreConfigSkeleton;
 
+class QAction;
+
 /*!
  * \class KCModuleData
  * \inmodule KCMUtilsCore
@@ -50,6 +52,26 @@ public:
      */
     virtual bool matchesQuery(const QString &query) const;
 
+    /*!
+     * \brief An auxiliary action associated with this KCM.
+     *
+     * \since 6.18
+     */
+    QAction *auxiliaryAction() const;
+
+    /**
+     * \brief Whether this KCM is relevant in the current environment.
+     *
+     * The displaying application might not show this KCM if it is
+     * not relevant. For example, a KCM for configuring touchscreens
+     * could be hidden if no touchscreen is present.
+     *
+     * Default is true.
+     *
+     * \since 6.18
+     */
+    bool isRelevant() const;
+
 Q_SIGNALS:
     /*!
      * \brief Emitted when KCModuleData is loaded.
@@ -66,6 +88,49 @@ Q_SIGNALS:
      * disconnect this signal in the derived constructor.
      */
     void aboutToLoad(QPrivateSignal);
+
+    /*!
+     * \brief Emitted when the auxiliary action changes.
+     *
+     * \note The QAction can change its state (e.g. being enabled)
+     * without the action object itself being replaced.
+     *
+     * \since 6.18
+     */
+    void auxiliaryActionChanged(QAction *action);
+
+    /*!
+     * \brief Emitted when the relevancy of this KCM changes.
+     *
+     * For example, after (un)plugging a relevant device.
+     *
+     * \since 6.18
+     */
+    void relevantChanged(bool relevant);
+
+protected:
+    /*!
+     * \brief Sets an auxiliary action for this KCM.
+     *
+     * This can be displayed alongside this KCM in a list of settings
+     * and provide quick access to a feature in the KCM without opening
+     * it.
+     *
+     * For example, a "Pair new device" action or, in case of a checkable
+     * action, a Switch to turn on/off a certain feature or device.
+     */
+    void setAuxiliaryAction(QAction *action);
+
+    /*!
+     * \brief Sets whether this KCM is relevant in the current environment.
+     *
+     * This can hide the KCM in a list of settings when it is currently
+     * not relevant.
+     *
+     * For example to show touchscreen settings only when a touchscreen
+     * is present. Default is true.
+     */
+    void setRelevant(bool relevant);
 
 protected Q_SLOTS:
     /*!
